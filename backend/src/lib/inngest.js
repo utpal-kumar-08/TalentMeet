aedd78ac0596ad721f48429414523cb7815f6c6a
Utpal Kumar <um697893@gmail.com>
2026-02-09 02:41:02 +0530

    feat: Add Stream integration for chat and video calls

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -2,7 +2,6 @@ import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
 import { deleteStreamUser, upsertStreamUser } from "./stream.js";
-
 export const inngest = new Inngest({ id: "talent-iq" });
 
 const syncUser = inngest.createFunction(
