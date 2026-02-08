41d19d51e5cc40c5d78b1fc562c8281043b99b6e
Utpal Kumar <um697893@gmail.com>
2026-02-08 23:26:28 +0530

    auth and db sync

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,6 +5,9 @@ import {ENV} from "./env.js";
 
 export const connectDB=async()=>{
     try {
+        if(!ENV.DB_URL){
+            throw new Error("❌DB URL is not defined")
+        }
         const conn=await mongoose.connect(ENV.DB_URL)
         console.log("✅DB connected",conn.connection.host)
     } catch (error) {
--- backend/src/server.js
+++ backend/src/server.js
@@ -13,10 +13,21 @@ import express from "express"
 import path from "path"
 import { ENV } from "./lib/env.js"   // ✅ FIXED PATH
 import { connectDB } from "./lib/db.js"
+import { inngest, functions } from "./lib/inngest.js"
+import cors from "cors"
+import {serve} from "inngest/express"
 
 const app = express()
 const __dirname = path.resolve()
 
+app.use(express.json())
+
+app.use(cors({
+  origin:ENV.CLIENT_URL,
+  credentials:true
+}))
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 console.log(ENV.DB_URL)
 console.log(ENV.PORT)
 
